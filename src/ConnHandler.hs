15ca61c620059d9783aba8f04bc46be74302bdbd
Sebastian Sturm <sebasian-sturm@hotmail.com>
2017-07-15 13:37:44 +0200

    Designfehler verbessert.

--- src/ConnHandler.hs
+++ src/ConnHandler.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE TypeFamilies #-}
 module ConnHandler where
 
 import Data.ByteString
 
-class ConnHandler h where
-       cGetSome :: h -> Int -> IO ByteString
-       cPut :: h -> ByteString -> IO ()
-       cFlush :: h -> IO ()
-       cClose :: h -> IO ()
+class (Monad m) => ConnMonad m where
+       type ConnHandler :: *
+       cGetSome :: ConnHandler -> Int -> m ByteString
+       cPut :: ConnHandler -> ByteString -> m ()
+       cFlush :: ConnHandler -> m ()
+       cClose :: ConnHandler -> m ()
 
